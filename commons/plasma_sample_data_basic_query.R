c15f3b68208fb34efcb0a72d3697a9918852fa49
juhaa <juhaa@users.noreply.github.com>
2024-10-16 09:35:54 +0300

    Add roxygen style documentation

--- commons/plasma_sample_data_basic_query.R
+++ commons/plasma_sample_data_basic_query.R
@@ -1,12 +1,53 @@
-# Example script how to combine all plasma availability info with endpoint data
-# and perform some basic queries
+#' @title Plasma Sample Data Basic Query
+#' @description This script combines plasma availability information with endpoint data and performs basic queries.
+#' @details The script performs the following tasks:
+#' - Authenticates and sets up the BigQuery project.
+#' - Defines a function to calculate approximate event dates given birth dates and ages at events.
+#' - Gathers sample annotations from a BigQuery table.
+#' - Reads plasma sample availability data from multiple files and combines them.
+#' - Processes and cleans the combined plasma sample data.
+#' - Retrieves phenotype data for specific endpoints from a BigQuery table.
+#' - Adds additional kidney disease sample information.
+#' - Merges phenotype data with plasma sample data.
+#' - Performs an example query to get unique individuals who meet specific criteria related to plasma samples and endpoints.
+#' 
+#' @section General Settings:
+#' - `projectid`: The BigQuery project ID (Sandbox name; Check the URL in your web browser).
+#' - `options(gargle_oauth_cache = FALSE)`: Disables OAuth cache.
+#' - `bq_auth(scopes = "https://www.googleapis.com/auth/bigquery")`: Authenticates BigQuery access.
+#' 
+#' @section Functions:
+#' - `calc_approx_date(date, event_age)`: Calculates approximate event dates given birth dates and ages at events.
+#' 
+#' @section Data Gathering:
+#' - Retrieves sample annotations from the `finngen-production-library.sandbox_tools_r12.minimum_extended_r12_v1` table.
+#' - Reads plasma sample availability data from multiple files and combines them.
+#' - Processes and cleans the combined plasma sample data.
+#' 
+#' @section Phenotype Data:
+#' - Retrieves phenotype data for specific endpoints from the `finngen-production-library.sandbox_tools_r12.endpoint_cohorts_r12_v1` table.
+#' - Adds additional kidney disease sample information from a local file.
+#' 
+#' @section Data Merging:
+#' - Merges phenotype data with plasma sample data.
+#' 
+#' @section Example Query:
+#' - Retrieves unique individuals who:
+#'   - Have the endpoint `ILD_ENDPOINTS`.
+#'   - Have plasma samples available.
+#'   - Have plasma samples frozen within 8 hours of collection.
+#'   - Have plasma samples taken before the first diagnosis.
+#'   - Have plasma samples taken after 2013.
+#' 
+#' @import bigrquery
+#' @import data.table
+#' @import lubridate
+#' @import ggplot2
 
 library(bigrquery)
 library(data.table)
 library(lubridate)
 library(ggplot2)
-library(bigrquery)
-
 
 # General settings
 
@@ -15,7 +56,21 @@ projectid <- "fg-production-sandbox-6"
 options(gargle_oauth_cache = FALSE)
 bq_auth(scopes = "https://www.googleapis.com/auth/bigquery")
 
-# Calculate approximate event date given birth date and age at event
+#' Calculate Approximate Event Date
+#'
+#' This function calculates the approximate event date given a birth date and age at the event.
+#'
+#' @param date A date object or a character string representing the birth date.
+#' @param event_age A numeric value representing the age at the event.
+#'
+#' @return A date object representing the approximate event date.
+#'
+#' @examples
+#' calc_approx_date("2000-01-01", 20.5)
+#' calc_approx_date(as.Date("2000-01-01"), 20.5)
+#'
+#' @importFrom lubridate ymd years days
+#' @export
 calc_approx_date <- function(date, event_age) {
   if (is.character(date)) {
     date <- ymd(date)
@@ -46,7 +101,6 @@ sql <- paste0(
 anno <- as.data.table(bq_table_download(bq_project_query(projectid, sql)))
 anno[, IN_DF12_ANALYSIS := ! is.na(batch)]
 
-
 # Read in plasma sample availability data
 
 F10_1 <- fread("/finngen/library-red/task_force_data/plasma_availability/F10/data/F10_1.txt")
@@ -69,7 +123,6 @@ RR <- merge(RR, anno, by = "FINNGENID", all = T)
 RR[is.na(AVAILABILITY), ":="(AVAILABILITY = "NO", FINNGENID_N_PLASMA_SAMPLES = 0)]
 RR[, BIOBANK := gsub("^THL BIOBANK.*", "THL BIOBANK", COHORT)]
 RR[, BIOBANK := gsub("^ARCTIC BIOBANK.*", "ARCTIC BIOBANK", BIOBANK)]
-
 
 # Get phenotype data
 
